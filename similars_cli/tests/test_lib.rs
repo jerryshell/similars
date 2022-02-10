69e9c1ccabebe3ba85254d06217691aa91ab841a
Jerry <calljerryli@outlook.com>
2022-02-10 17:07:48 +0800

    Add unit test

---
+++ similars_cli/tests/test_lib.rs
@@ -0,0 +1,25 @@
+#[test]
+fn test_get_image_distance_by_path1() {
+    let distance = similars_lib::get_image_distance_by_path(
+        "example_img1.jpg",
+        "example_img2.jpg",
+        8,
+        8,
+        false,
+    )
+    .unwrap();
+    assert_eq!(distance, 3)
+}
+
+#[test]
+fn test_get_image_distance_by_path2() {
+    let distance = similars_lib::get_image_distance_by_path(
+        "example_img1.jpg",
+        "example_img1.jpg",
+        8,
+        8,
+        false,
+    )
+    .unwrap();
+    assert_eq!(distance, 0)
+}
